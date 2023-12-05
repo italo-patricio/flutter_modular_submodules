b44c3168cf3fef86979765589016c393a33ed15a
Tech <tech@elo7.com>
2023-12-05 16:12:24 -0300

    rename file module

--- lib/modules/app/app_module.dart
+++ lib/modules/app/app_module.dart
@@ -1,4 +1,4 @@
-import 'package:estudo_modulo/modules/app/awesome_submodule/other_submodule.dart';
+import 'package:estudo_modulo/modules/app/awesome_submodule/awesome_submodule.dart';
 import 'package:estudo_modulo/modules/app/other_submodule/other_submodule.dart';
 import 'package:estudo_modulo/modules/core/core_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
